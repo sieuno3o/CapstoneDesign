87c3fdbe3eb1451e3858e9a9e13b691005d8d160
Juseop <monal100@naver.com>
2026-04-23 17:30:40 +0900

    Create run_rf.py

---
+++ run_rf.py
@@ -0,0 +1,7 @@
+from src.train_ai_pipeline import train_ai_pipeline
+
+if __name__ == "__main__":
+    train_ai_pipeline(
+        "hanwha_aerospace",
+        "data/rawdata/hanwha_aerospace_5y.csv"
+    )
